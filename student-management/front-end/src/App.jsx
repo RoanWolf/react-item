ebf4f336382db2c8e5a249826af1f7e0701db28b
RoanWolf <neq17320251@gmail.com>
2025-10-03 23:51:56 +0800

    navbar create init

--- student-management/front-end/src/App.jsx
+++ student-management/front-end/src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./UI/layout";
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Layout />}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
