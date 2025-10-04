c0e1076d19279197c49af8ede7fe49a7bfc37962
Angel <neq17320251@gmail.com>
2025-10-04 23:56:57 +0800

    relax

--- student-management/front-end/src/App.jsx
+++ student-management/front-end/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./UI/layout";
+import Avator from "./UI/Avator";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Layout />}></Route>
-        </Routes>
+        <Route path="/" element={<Layout />}>
+        
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
